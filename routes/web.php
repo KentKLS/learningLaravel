0b2c121bb6ec2fac83d0520304d139382c58e024
Quentin <kent.26780@gmail.com>
2023-06-09 12:10:17 +0200

    Added new route as asked in Exercice 2.1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Homepage';
+});
+Route::get('/product', function () {
+    return 'Liste des produits';
+});
+Route::get('/product/{id}', function ($id) {
+    return "Fiche du produit ".$id;
+});
+Route::get('/cart', function () {
+    return 'Panier' ;
 });
