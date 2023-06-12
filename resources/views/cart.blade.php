70ab8b3951a310ebec2a76d55dfc81796a1f5fbe
Quentin <kent.26780@gmail.com>
2023-06-12 09:52:23 +0200

    added an header and a footer reusable element

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,12 +1,94 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    Panier
-</body>
-</html>
+@include('header')
+<div class="mainContainer contentIsCentered isFullPage">
+    <div class="tableContainer">
+        <form action="" method="POST">
+            <table>
+                <tr>
+                    <th>Produit</th>
+                    <th>Prix Unitaire</th>
+                    <th>Quantité</th>
+                    <th>Total</th>
+                    <th></th>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td> </td>
+                    <td>
+                        <input class="numberInput" max="99" type="number" name="numberOrdered[]" min="0"
+                            value="">
+                    </td>
+                    <td< /td>
+                        <td>
+                            <input type="hidden" name="product[]" value="">
+
+                        </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td>Total HT</td>
+                    <td> </td>
+
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td>TVA</td>
+                    <td> %</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td>Total TTC</td>
+                    <td></td>
+                </tr>
+
+            </table>
+            <button>Mettre à jour tableau</button>
+
+
+    </div>
+
+
+
+
+    <div class="horizontalDiv">
+        <label for="transporter">Transporteur:</label>
+        <select name="transporter">
+            <option value="laPoste">La Poste</option>
+            <option value="chronoPost">ChronoPost</option>
+        </select>
+        <button>VALIDER</button>
+
+    </div>
+    <table>
+        <tr>
+
+            <td>TRANSPORT</td>
+            <td></td>
+
+        </tr>
+        <tr>
+
+            <td>Total TTC</td>
+            <td></td>
+
+        </tr>
+
+    </table>
+    </form>
+    <form action="" method="POST">
+        <button>Vidé le panier</button>
+        <input type="hidden" name="emptyCart" value="0">
+
+    </form>
+    <form action="" method="POST">
+        <button>Validé Commande</button>
+        <input type="hidden" name="orderPassed" value="0">
+
+    </form>
+</div>
+
+
+@include('footer')
--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -1,12 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    liste des produits
-</body>
-</html>
+@include('header')
+
+<form action="/cart" method="POST">
+    <section class="mainContainer">
+        <div class="cardContainer">
+            <div class="productCard">
+                <div class="cardImgContainer">
+                    <img src=''>
+                </div>
+                <h3> </h3>
+                <p> Prix : </p>
+                <p>Poids : g</p>
+                <p>Price after % discount :</p>
+                <label for="numberOrdered[]">Quantité :</label>
+                <input type="hidden" name="product[]" value="">
+                <input type="number" class="numberInput" value="" min="0" max="99"
+                    name="numberOrdered[]">
+                <button class="addToCart">Ajoutez au panier</button>
+            </div>
+        </div>
+        <div class="containerFlexCentered">
+
+        </div>
+    </section>
+</form>
+
+@include('footer')
